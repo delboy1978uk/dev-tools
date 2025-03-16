6715f82a4d64d597c74ea5f827f2d0ad25707a6f
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-03-16 18:46:07 +0100

    folder and psr-4 restructure

---
+++ src/Codeception/Module/ApiValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codeception\Module;
+
+use Codeception\Module;
+
+class ApiValidator extends Module
+{
+    public function seeValidApiSpec(string $operation, string $route, string $responseBody): void
+    {
+
+    }
+}
