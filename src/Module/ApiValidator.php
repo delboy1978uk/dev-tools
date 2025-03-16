69c68b15e7325485e9c4d1c262f869c5422b5fbe
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-03-16 16:56:34 +0100

    add api validator in codeception module namespace

---
+++ src/Module/ApiValidator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codeception\Module;
+
+class ApiValidator extends Module
+{
+    public function validateApiSpec(string $operation, string $route, string $responseBody): void
+    {
+
+    }
+}
